2b349cc24edee035c38110623136f0714e678de9
animx11 <animx11@gmail.com>
2020-12-16 23:56:46 +0100

    hotfix

--- Ex3/Alice.rb
+++ Ex3/Alice.rb
@@ -6,18 +6,18 @@ address_ip = gets.to_s.strip
 
 socket = TCPSocket.new(address_ip,7777)
 
-dsa = OpenSSL::PKey::DSA.new(2048)
 rsa = OpenSSL::PKey::RSA.new(2048)
+dsa = OpenSSL::PKey::DSA.new(2048)
 
-rsa_pub_key_pem = dsa.public_key.to_pem
-dsa_pub_key_pem = rsa.public_key.to_pem
+rsa_pub_key_pem = rsa.public_key.to_pem
+dsa_pub_key_pem = dsa.public_key.to_pem
 
 digest = OpenSSL::Digest::SHA1.digest(rsa_pub_key_pem)
 sig = dsa.syssign(digest)
 
+
 socket.puts dsa_pub_key_pem.unpack("B*")
 socket.puts rsa_pub_key_pem.unpack("B*")
-socket.puts digest.unpack("B*")
 socket.puts sig.unpack("B*")
 
 socket.close
--- Ex3/Bob.rb
+++ Ex3/Bob.rb
@@ -6,15 +6,12 @@ client = socket.accept
 
 alice_dsa_pub_key_pem = [client.gets.gsub(/\n$/, '')].pack("B*")
 alice_rsa_pub_key_pem = [client.gets.gsub(/\n$/, '')].pack("B*")
-alice_digest = [client.gets.gsub(/\n$/, '')].pack("B*")
 alice_sig = [client.gets.gsub(/\n$/, '')].pack("B*")
 
 dsa = OpenSSL::PKey::DSA.new(alice_dsa_pub_key_pem)
 digest = OpenSSL::Digest::SHA1.digest(alice_rsa_pub_key_pem)
 
 puts dsa.sysverify(digest, alice_sig)
-puts dsa.sysverify(alice_digest, alice_sig)
-
 
 
 socket.close
