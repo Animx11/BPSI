128650f333084745f7777b1738756f9e02bff158
animx11 <animx11@gmail.com>
2020-12-16 23:29:27 +0100

    Veryfying Alice rsa pub key by Bob

--- Ex3/Bob.rb
+++ Ex3/Bob.rb
@@ -8,7 +8,10 @@ alice_dsa_pub_key_pem = [client.gets.gsub(/\n$/, '')].pack("B*")
 alice_rsa_pub_key_pem = [client.gets.gsub(/\n$/, '')].pack("B*")
 alice_sig = [client.gets.gsub(/\n$/, '')].pack("B*")
 
+dsa = OpenSSL::PKey::DSA.new(2048)
+digest = OpenSSL::Digest::SHA1.digest(alice_rsa_pub_key_pem)
 
+puts dsa.sysverify(digest, alice_sig)
 
 
 socket.close
