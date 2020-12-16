0c2c6bd17828871e93efa07be951bf883d287bd1
animx11 <animx11@gmail.com>
2020-12-16 23:36:05 +0100

    hotfix

--- Ex3/Bob.rb
+++ Ex3/Bob.rb
@@ -9,7 +9,7 @@ alice_rsa_pub_key_pem = [client.gets.gsub(/\n$/, '')].pack("B*")
 alice_digest = [client.gets.gsub(/\n$/, '')].pack("B*")
 alice_sig = [client.gets.gsub(/\n$/, '')].pack("B*")
 
-dsa = OpenSSL::PKey::DSA.new(2048)
+dsa = OpenSSL::PKey::DSA.new(alice_dsa_pub_key_pem)
 digest = OpenSSL::Digest::SHA1.digest(alice_rsa_pub_key_pem)
 
 puts dsa.sysverify(digest, alice_sig)
