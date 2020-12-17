75bad609a52a69de2d7f88ff8ea816faf4f216bb
animx11 <animx11@gmail.com>
2020-12-17 13:26:36 +0100

    Final version of Ex3

--- Ex3/Alice.rb
+++ Ex3/Alice.rb
@@ -6,22 +6,29 @@ address_ip = gets.to_s.strip
 
 socket = TCPSocket.new(address_ip,7777)
 
+puts "Creating RSA and DSA key pairs"
 rsa = OpenSSL::PKey::RSA.new(2048)
 dsa = OpenSSL::PKey::DSA.new(2048)
 
 rsa_pub_key_pem = rsa.public_key.to_pem
 dsa_pub_key_pem = dsa.public_key.to_pem
 
+puts "Creating hash from RSA public key"
 digest = OpenSSL::Digest::SHA1.digest(rsa_pub_key_pem)
+puts "Signing RSA public key"
 sig = dsa.syssign(digest)
 
-
+puts "Sending DSA and RSA public keys and signed RSA public key"
 socket.puts dsa_pub_key_pem.unpack("B*")
 socket.puts rsa_pub_key_pem.unpack("B*")
 socket.puts sig.unpack("B*")
-
+puts "Waiting for Bob encrypted message"
 encrypted_message = [socket.gets.gsub(/\n$/, '')].pack("B*")
+puts "Received encrypted message"
 
-rsa.private_decrypt(encrypted_message)
+puts "Decrypted message: " + rsa.private_decrypt(encrypted_message)
 
 socket.close
+
+puts "Disconnecting socket"
+puts "Disabling the program"
--- Ex3/Bob.rb
+++ Ex3/Bob.rb
@@ -2,22 +2,29 @@ require 'openssl'
 require 'socket'
 
 socket = TCPServer.new(7777)
+puts "Waiting for connection"
 client = socket.accept
 
+puts "Connection established"
 alice_dsa_pub_key_pem = [client.gets.gsub(/\n$/, '')].pack("B*")
 alice_rsa_pub_key_pem = [client.gets.gsub(/\n$/, '')].pack("B*")
 alice_sig = [client.gets.gsub(/\n$/, '')].pack("B*")
+puts "Received DSA and RSA public keys and signed RSA public key"
 
+puts "Creating hash from received RSA public key"
 dsa = OpenSSL::PKey::DSA.new(alice_dsa_pub_key_pem)
 digest = OpenSSL::Digest::SHA1.digest(alice_rsa_pub_key_pem)
-
+puts "Verifying signed RSA public key"
 ver = dsa.sysverify(digest, alice_sig)
 
 if ver
   puts "Sign is correct, sending message to Alice"
-  message = "Message to send"
+  puts "Please write message to send"
+  message = gets
   rsa = OpenSSL::PKey::RSA.new(alice_rsa_pub_key_pem)
+  puts "Encrypting message with Alice RSA public key"
   encrypted_message = rsa.public_encrypt(message)
+  puts "Sending message"
   client.puts encrypted_message.unpack("B*")
   puts "Message was correctly encrypted and sent to Alice"
 
@@ -26,3 +33,5 @@ else
 end
 
 socket.close
+puts "Disconnecting server"
+puts "Disabling the program"
